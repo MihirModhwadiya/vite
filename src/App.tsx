584e4ae098aecd436c672a31fc1277763458f82e
MihirModhwadiya <mihirmodhwadiya21@gmail.com>
2023-08-29 17:18:28 +0530

    const delete

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const con = 9;
-
-  console.log(con);
-  
-
-
+  const [count, setCount] = useState(0);
   return (
     <>
       <div>
@@ -35,7 +28,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
